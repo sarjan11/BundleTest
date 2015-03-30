c005790f208b53e82ad03976389343766c936759
sarjan <sarznshrestha@gmail.com>
2015-03-29 22:26:30 -0400

    BundleActivator class added

--- BundleTest/src/com/example/bundle/BundleExample.java
+++ BundleTest/src/com/example/bundle/BundleExample.java
@@ -1,5 +1,7 @@
 package com.example.bundle;
 
 public class BundleExample {
-
+	public static void main(String[] args) {
+		System.out.println("need to add activator module");
+	}
 }
---
+++ BundleTest/src/com/example/bundle/activator/Activator.java
@@ -0,0 +1,5 @@
+package com.example.bundle.activator;
+
+public class Activator {
+
+}
